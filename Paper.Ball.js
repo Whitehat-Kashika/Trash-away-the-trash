6cd3d85fb2580f0696776f25ed67c43d208ab745
Kashika Vedasen <74352677+Whitehat-Kashika@users.noreply.github.com>
2020-12-12 14:47:53 +0530

    Add files via upload

---
+++ Paper.Ball.js
@@ -0,0 +1,24 @@
+class Ball {
+    constructor(x,y,r) {
+      var options = {
+          isStatic:false,
+          restitution:0.8,
+            friction:0.3,
+          density:1.2
+      }
+
+      this.body = Bodies.circle(this.x, this.r/2, 15, options);
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+
+      push();
+      translate(pos.x, pos.y);
+      rectMode(CENTER);
+      fill(255, 0, 255);
+      ellipse(0,0, this.r, this.r);
+      pop();
+    }
+  }
